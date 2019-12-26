ed71f9b978ae316ecd32e0aebb46c3f87279fce3
Jan Vermeir <jvermeir@hotmail.com>
2019-12-26 21:09:30 +0100

    eerste versie

---
+++ opdrachten/lijsten.py
@@ -0,0 +1,65 @@
+'''
+oefeningen met lijsten en loops
+
+algemeen algorithme:
+
+1. definieer een of meer variabelen waarin het eindresultaat opgeslagen wordt.
+2. bekijk elk element van de lijst en bepaal of je het eindresultaat aan moet passen
+3. print het eindresultaat
+'''
+
+
+
+# 1.
+# input
+woorden = ['noot', 'mies', 'tomatensoep', 'aap']
+'''
+vraag:
+    print elk woord en de lengte er van op aparte regels
+resultaat:
+    noot: 4
+    mies: 4
+    tomatensoep: 11 
+    aap: 3
+'''
+'''
+bonusvraag: 
+    wat gebeurt er als woorden gelijk is aan de lege lijst?
+    wat gebeurt er als woorden de waarde None heeft?
+'''
+
+# 2.
+# input
+woorden = ['noot', 'mies', 'tomatensoep', 'aap']
+'''
+vraag:
+    wat is de lengte van het langste woord?
+    wat is het langste woord?
+resultaat:
+'''
+lengte_langste_woord = 7
+langste_woord = 'tomatensoep'
+
+# 3.
+# input
+cijfers = [1, 3, 5, 7, 9, 2]
+'''    
+vraag:
+    wat is het hoogste cijfer
+resultaat:
+'''
+het_hoogste_cijfer = 9
+
+# 4.
+# input
+hoogste_cijfer = [3, 5, 6, 10]
+laagste_cijfer = [1, 2, 2, 9]
+'''
+vraag:
+    wat is het grootste verschil tussen twee paren van hoogste_cijfer/laagste_cijfer?
+    alle hoogste/laagste paren zijn:
+    (3,1) (5,2) (6,2) (10,9)
+resultaat:
+'''
+het_grootste_verschil = 4
+
